fe6befecd2fe4670d217fba320811251c1391d75
Mayco Neves <mayconeves@gmail.com>
2023-08-10 11:46:52 -0300

    Resolucao

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,59 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { INICIANTE, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class Usuario(val nome: String, val id: Long)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val stack: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    infix fun matricular(usuario: Usuario) { inscritos.add(usuario) }
+
+    fun matricular(vararg usuarios: Usuario){
+        for(usuario in usuarios)
+            inscritos.add(usuario)
+    }
+
+    fun mostrarConteudos(){
+        for(conteudo in conteudos)
+            println("Conteúdo: ${conteudo.nome}\t Módulo: ${conteudo.stack} \t Duração: ${conteudo.duracao}")
+    }
+
+    fun mostrarInscritos(){
+        for(inscrito in inscritos)
+            println(inscrito.nome)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando os usuários
+    val usuario1 = Usuario("Joao", 1L)
+    val usuario2 = Usuario("Maria", 2L)
+    val usuario3 = Usuario("Jose", 3L)
+    val usuario4 = Usuario("Marcos", 4L)
+    val usuario5 = Usuario("Mateus", 5L)
+
+    // Criando a lista de conteúdos
+    val listaConteudosKotlin: List<ConteudoEducacional> = listOf(
+        ConteudoEducacional("Desmistificando a Linguagem de Programação Kotlin","Kotlin", 60),
+        ConteudoEducacional("Fundamentos de Desenvolvimento Mobile Nativo Para Android","Kotlin", 48),
+        ConteudoEducacional("Noções Básicas do Android com Kotlin","Kotlin", 120),
+    	ConteudoEducacional("Dominando o Android Jetpack","Fundamentos Kotlin", 80))
+
+    // Criando a formação
+    val formacao = Formacao("Formação Android Developer", listaConteudosKotlin, Nivel.INICIANTE)
+    
+    // Verificando se a variável de formação não é nula, caso não seja, matriculando alunos
+    formacao?.let{
+        formacao.matricular(usuario1, usuario2, usuario3)
+    }
+
+    // Imprimindo na tela
+      println("Formação:\n${formacao.nome}\n")
+    println("Conteúdos:")
+    formacao.mostrarConteudos()
+    println("\nInscritos:")
+    formacao.mostrarInscritos()
+
 }
